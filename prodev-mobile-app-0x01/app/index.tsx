3c45a4d9d886fec2dfe6a8a08ee3e02c9d7541ee
Opeyemi <eco.ajibade@gmail.com>
2025-10-30 17:52:37 +0100

    Define Your Styles

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -1,22 +1,5 @@
 import { Text, View, StyleSheet } from "react-native";
 
-export default function Index() {
-  return (
-    <View style={styles.container}>
-      <Text>Entry Screen - Awesome</Text>
-      <View>
-        <Text style={styles.largeText}>
-          Typescript is great if you practice more
-        </Text>
-        <Text style={styles.mediumText}>
-          React Native provides you a single codebase for cross platforms
-        </Text>
-        <Text style={styles.smallText}>ALX is awesome</Text>
-      </View>
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#90caf9",
@@ -42,3 +25,18 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
 });
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>Entry Screen - Awesome</Text>
+      <Text style={styles.largeText}>
+        Typescript is great if you practice more
+      </Text>
+      <Text style={styles.mediumText}>
+        React Native provides you a single codebase for cross platforms
+      </Text>
+      <Text style={styles.smallText}>ALX is awesome</Text>
+    </View>
+  );
+}
